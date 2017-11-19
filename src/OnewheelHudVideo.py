0d4d4f94b2fce61af117fbfd0fb65bd4642badc7
Walther Maciel <walthermaciel@gmail.com>
2017-11-19 12:38:51 -0800

    PEP8

--- src/OnewheelHudVideo.py
+++ src/OnewheelHudVideo.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-from moviepy.editor import *
-from IconManager import IconManager
 import csv
 from datetime import datetime
+
 import tqdm
+from moviepy.editor import *
+
+from IconManager import IconManager
 
 resolution_map = {
     '1080': {
@@ -16,11 +18,13 @@ resolution_map = {
     }
 }
 
+
 class OnewheelHudVideo:
-    def __init__(self, data_path, footage_path, orientation='portrait', resolution='1080', start_second=0, skip_rows=0, length=None, show_time=False):
+    def __init__(self, data_path, footage_path, orientation='portrait', resolution='1080', start_second=0, skip_rows=0,
+                 length=None, show_time=False):
         self.footage_path = footage_path
         self.orientation = orientation
-        self.resolutions = self.compute_resolutions(orientation, resolution)
+        self.resolutions = compute_resolutions(orientation, resolution)
         self.start_second = start_second
         self.length = length
         self.icon_manager = IconManager(resolution=res_2_tuple(self.resolutions['icon']))
@@ -53,10 +57,10 @@ class OnewheelHudVideo:
             final_clip = final_clip.subclip(t_end=self.length)
 
         print 'Rendering...'
-        #final_clip.save_frame('frame.png', t=0)
-        #final_clip.resize(0.4).preview(fps=15, audio=False)
-        #final_clip.write_videofile("onewheel.MP4", fps=60, threads=8)
-        #info_clip.preview(fps=60, audio=False)
+        # final_clip.save_frame('frame.png', t=0)
+        # final_clip.resize(0.4).preview(fps=15, audio=False)
+        # final_clip.write_videofile("onewheel.MP4", fps=60, threads=8)
+        # info_clip.preview(fps=60, audio=False)
         info_clip.write_videofile('info.MP4', fps=30, threads=8)
 
     def generate_info_clip(self):
@@ -75,17 +79,22 @@ class OnewheelHudVideo:
                 i += 1
             delta_seconds = self.compute_log_delay(i)
 
-            icon_clips['speed'].append(self.icon_manager.get_speed_icon_clip(speed=row['speed'], duration=delta_seconds))
-            icon_clips['pitch'].append(self.icon_manager.get_pitch_icon_clip(angle=row['pitch'], duration=delta_seconds))
-            icon_clips['roll'].append(self.icon_manager.get_roll_icon_clip(angle=row['roll'], duration=delta_seconds))
-            icon_clips['battery'].append(self.icon_manager.get_battery_icon_clip(charge=row['battery'], duration=delta_seconds))
-            icon_clips['temperature'].append(self.icon_manager.get_temperature_icon_clip(temperature=row['motor_temp'], duration=delta_seconds))
+            icon_clips['speed'].append(self.icon_manager.get_speed_icon_clip(speed=row['speed'],
+                                                                             duration=delta_seconds))
+            icon_clips['pitch'].append(self.icon_manager.get_pitch_icon_clip(angle=row['pitch'],
+                                                                             duration=delta_seconds))
+            icon_clips['roll'].append(self.icon_manager.get_roll_icon_clip(angle=row['roll'],
+                                                                           duration=delta_seconds))
+            icon_clips['battery'].append(self.icon_manager.get_battery_icon_clip(charge=row['battery'],
+                                                                                 duration=delta_seconds))
+            icon_clips['temperature'].append(self.icon_manager.get_temperature_icon_clip(temperature=row['motor_temp'],
+                                                                                         duration=delta_seconds))
         print 'Combining icons...'
         info_clip = self.combine_info_clips(icon_clips)
         return info_clip
 
     def compute_log_delay(self, i):
-        delta_t = self.data[i+1]['time'] - self.data[i]['time']
+        delta_t = self.data[i + 1]['time'] - self.data[i]['time']
         delta_seconds = delta_t.seconds + delta_t.microseconds * 1e-6
         return delta_seconds
 
@@ -122,12 +131,11 @@ class OnewheelHudVideo:
             # make a vertical bar
             return clips_array(vertical_array)
         else:
-            raise Exception ("Orientation not set")
-
+            raise Exception("Orientation not set")
 
     def generate_footage_clip(self):
         footage_clip = (VideoFileClip(self.footage_path)
-            .resize(res_2_tuple(self.resolutions['footage'])))
+                        .resize(res_2_tuple(self.resolutions['footage'])))
 
         # if self.length is not None:
         #     footage_clip = footage_clip.subclip(t_end=self.length)
@@ -137,30 +145,32 @@ class OnewheelHudVideo:
 
         return footage_clip
 
-    def compute_resolutions(self, orientation, resolution_name):
-        resolution = resolution_map[resolution_name][orientation]
-        resolutions = {}
-        n_icons = 5
 
-        # the footage is the same as the full resolution
-        resolutions['footage'] = resolution
+def compute_resolutions(orientation, resolution_name):
+    resolution = resolution_map[resolution_name][orientation]
+    resolutions = {}
+    n_icons = 5
 
-        # in portrait, the icons will be layed along the bottom edge
-        if orientation == 'portrait':
-            resolutions['icon'] = {
-                'w': resolution['w'] / float(n_icons),
-                'h': resolution['w'] / float(n_icons)
-            }
-        # in landscape, the icons will be layed along the right edge
-        else: # orientation == landscape
-            resolutions['icon'] = {
-                'w': resolution['h'] / float(n_icons),
-                'h': resolution['h'] / float(n_icons)
-            }
+    # the footage is the same as the full resolution
+    resolutions['footage'] = resolution
 
-        return resolutions
+    # in portrait, the icons will be layed along the bottom edge
+    if orientation == 'portrait':
+        resolutions['icon'] = {
+            'w': resolution['w'] / float(n_icons),
+            'h': resolution['w'] / float(n_icons)
+        }
+    # in landscape, the icons will be layed along the right edge
+    else:  # orientation == landscape
+        resolutions['icon'] = {
+            'w': resolution['h'] / float(n_icons),
+            'h': resolution['h'] / float(n_icons)
+        }
 
-def mile2Km(mile):
+    return resolutions
+
+
+def mile_to_km(mile):
     """
     Converts Miles to Kilometers
     """
@@ -176,17 +186,19 @@ def f_to_c(f_temp):
     except ValueError:
         return None
 
+
 def parse_angle(angle_text, invert=False):
     """
     Converts the original angle to values between -180 and 180, with 0 being horizontal
     """
     try:
-        angle = float(angle_text)/10 - 180
+        angle = float(angle_text) / 10 - 180
         if invert:
             angle = -angle
         return angle
     except ValueError:
         return None
+
 
 def parse_logs(file_path, skip_rows=0):
     """
@@ -198,44 +210,50 @@ def parse_logs(file_path, skip_rows=0):
         log_reader = csv.DictReader(logfile)
         for row in log_reader:
             data.append({
-                'time':parse_milisecond_time(row['time']),
-                'speed':mile2Km(row['speed']),
-                'battery':int(row['battery']),
-                'roll':parse_angle(row['tilt_angle_roll'], invert=True),
-                'pitch':parse_angle(row['tilt_angle_pitch']),
-                'motor_temp':f_to_c(row['motor_temp'])
-                })
+                'time': parse_milisecond_time(row['time']),
+                'speed': mile_to_km(row['speed']),
+                'battery': int(row['battery']),
+                'roll': parse_angle(row['tilt_angle_roll'], invert=True),
+                'pitch': parse_angle(row['tilt_angle_pitch']),
+                'motor_temp': f_to_c(row['motor_temp'])
+            })
     print 'Loaded ', len(data), 'rows'
     return data[skip_rows:]
+
 
 def compute_average_delta_t(data):
     deltas_s = []
 
     for i, row in enumerate(data):
-        if i+1 < len(data):
-            delta_t = data[i+1]['time'] - row['time']
+        if i + 1 < len(data):
+            delta_t = data[i + 1]['time'] - row['time']
             deltas_s.append(round(delta_t.seconds + delta_t.microseconds * 1e-6, 2))
 
-    return sum(deltas_s)/len(deltas_s)
+    return sum(deltas_s) / len(deltas_s)
+
 
 def parse_original_time(time_str):
     # remove timezone info
     time_str = time_str[:-5]
     return datetime.strptime(time_str, '%Y-%m-%dT%H:%M:%S')
 
-def parse_milisecond_time(time_str): # "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+
+def parse_milisecond_time(time_str):  # "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
     # remove timezone info
     time_str = time_str[:-5]
     return datetime.strptime(time_str, '%Y-%m-%dT%H:%M:%S.%f')
+
 
 def res_2_tuple(resolution):
     """
     Converts a resolution map to a tuple
     """
-    return (resolution['h'], resolution['w'])
+    return resolution['h'], resolution['w']
+
 
 if __name__ == '__main__':
     import argparse
+
     parser = argparse.ArgumentParser(description='Generates a HUD video of your onewheel ride from a log file')
     parser.add_argument('log_file', type=str, help='Path to the logfile used to annotate the video')
     parser.add_argument('video_file', type=str, help='Path to the video to be annotated')
